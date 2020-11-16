df9bcdcfad420746e1da6ffe40cce66738a3068f
Mark <m.hern1415@gmail.com>
2020-11-16 10:59:16 -0800

    added migrations and seeds

--- challenge-api/db/schema.rb
+++ challenge-api/db/schema.rb
@@ -10,10 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_213843) do
+ActiveRecord::Schema.define(version: 2020_11_13_052435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alerts", force: :cascade do |t|
+    t.text "errorId"
+    t.text "errorSeverity"
+    t.text "errorCategory"
+    t.text "errorMessage"
+    t.text "longMessage"
+    t.bigint "errorTime"
+    t.boolean "selected"
+    t.boolean "new"
+    t.boolean "expanded"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.text "_id"
+    t.text "contactId"
+    t.text "contactStatus"
+    t.integer "contactName"
+    t.text "contactGround"
+    t.text "contactSatellite"
+    t.text "contactEquipment"
+    t.text "contactState"
+    t.text "contactStep"
+    t.text "contactDetail"
+    t.integer "contactBeginTimestamp"
+    t.integer "contactEndTimestamp"
+    t.float "contactLatitude"
+    t.float "contactLongitude"
+    t.float "contactAzimuth"
+    t.float "contactElevation"
+    t.text "contactResolution"
+    t.text "contactResolutionStatus"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
