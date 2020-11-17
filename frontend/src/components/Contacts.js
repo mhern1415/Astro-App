bb3f8c724f05b9fdb4f13e609d5191fd43e844a0
Mark <m.hern1415@gmail.com>
2020-11-17 11:06:20 -0800

    added state count

--- frontend/src/components/Contacts.js
+++ frontend/src/components/Contacts.js
@@ -25,20 +25,47 @@ class Contacts extends Component {
         sorted.sort((a,b) => (b[sortKey] - a[sortKey]))
         this.setState({sorted})
     }
+    onSortAscState(event, sortKey) {
+      const sorted = this.state.contacts
+      sorted.sort((a,b) => a[sortKey].localeCompare(b[sortKey]))
+      this.setState({sorted})
+    }
+    onSortDescState(event, sortKey) {
+      const sorted = this.state.contacts
+      sorted.sort((a,b) => b[sortKey].localeCompare(a[sortKey]))
+      this.setState({sorted})
+    }
+   
+    
     render() {
         const { contacts } = this.state;
         const contactSize = contacts.length
+        const states = []
+        contacts.map (contact =>(
+         states.push(contact.contactState)
+        ))
+        const counts1 = Object.create(null)
+        states.forEach(btn => {
+          counts1[btn] = counts1[btn] ? counts1[btn] + 1 : 1;
+        })
+        
+
 
         return (
-            <div>            
+            <div> 
                 <table class="rux-table" >
-                <caption><h1>{contactSize} Total contacts</h1></caption>
+                  <caption><h1>{contactSize} Total contacts</h1>
+                  <h1>{Object.entries(counts1).map(([key, val]) => 
+                    <h2 key={key}>{key}: {val}</h2>
+                    )}</h1>
+                  </caption>
                 <thead>
                   <tr>
                     <th>Contact Name<ArrowUpwardIcon onClick={e => this.onSortAsc(e, 'contactName')}/><ArrowDownwardIcon onClick={e => this.onSortDesc(e, 'contactName')} /></th>
                     <th>Contact Status</th>
                     <th>Begin Timestamp</th>
                     <th>End Timestamp</th>
+                    <th>Contact State<ArrowUpwardIcon onClick={e => this.onSortAscState(e, 'contactState')}/><ArrowDownwardIcon onClick={e => this.onSortDescState(e, 'contactState')} /></th>
                   </tr>
                 </thead>
                 <tbody>
@@ -48,6 +75,7 @@ class Contacts extends Component {
                       <td>{contact.contactStatus}</td>
                       <td>{contact.contactBeginTimestamp}</td>
                       <td>{contact.contactEndTimestamp}</td>
+                      <td>{contact.contactState}</td>
                     </tr>
                   ))}
                 </tbody>
